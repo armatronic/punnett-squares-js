7093621919c44be3606c26d14136a597f51e736a
armatron <amri.sannang@gmail.com>
2011-11-01 10:35:41 +1100

    Replaced module code with _.memoize()

--- src/application.js
+++ src/application.js
@@ -3,24 +3,9 @@
 // http://weblog.bocoup.com/organizing-your-backbone-js-application-with-modules
 var punnettSquares = {
     //
-    // Look @ _.memoize
-    // Create this closure to contain the cached modules
-    module: function() {
-        // Internal module cache.
-        var modules = {};
-
-        // Create a new module reference scaffold or load an
-        // existing module.
-        return function(name) {
-            // If this module has already been created, return it.
-            if (modules[name]) {
-                return modules[name];
-            }
-
-            // Create a module and save it under this name
-            return modules[name] = {};
-        };
-    }()
+    // Module, when called with a name, creates a Backbone module which is
+    // memoized.
+    module: _.memoize(function(name) { return {}; })
 };
 
 (function($) {
